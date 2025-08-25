078ee7420c4c6f4f05bbb33cf3e426ffb0a15f7c
hechen <hechen@etg.global>
2025-08-25 17:38:29 +0200

    cc

---
+++ DI_LiveCycle/DI_LiveCycle/Case1.cs
@@ -0,0 +1,72 @@
+﻿namespace DI_LiveCycle;
+
+public class Case1
+{
+    
+}
+
+
+public class Controller
+{
+    private readonly ILog log;
+    private readonly IStorage storage;
+
+    public Controller(ILog log, IStorage storage)
+    {
+        this.log = log;
+        this.storage = storage;
+    }
+    
+    public void Test()
+    {
+        this.log.Log("Test message");
+        this.storage.Save("Hello World", "test.txt");
+    }
+}
+
+public interface ILog
+{
+    public void Log(string msg);
+}
+
+public classLogImpl : ILog
+{
+    public void Log(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
+interface IConfig
+{
+    public string GetValue(string name);
+}    
+
+public class ConfigImpl : IConfig
+{
+    public string GetValue(string name)
+    {
+        return "value";
+    }
+}
+
+public interface IStorage
+{
+    public void Save(string content, string name);
+}
+
+public class StorageImpl : IStorage
+{
+    private readonly IConfig config;
+
+    public StorageImpl(IConfig config)
+    {
+        this.config = config;
+    }
+    
+    public void Save(string content, string name)
+    {
+        string server = config.GetValue("server");
+        Console.WriteLine($"server: {server}, content: {content}, name: {name}");
+    }
+}
---
+++ DI_LiveCycle/DI_LiveCycle/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+ServiceCollection services = new ServiceCollection();
+// services.AddTransient<TestServiceImpl>();
+// services.AddSingleton<TestServiceImpl>();
+// services.AddScoped<TestServiceImpl>();
+
+services.AddScoped<ITestService, TestServiceImpl>();
+using (ServiceProvider sp = services.BuildServiceProvider())
+{
+    /*
+    TestServiceImpl t = sp.GetService<TestServiceImpl>();
+    t.Name = "Test";
+    t.SayHi();
+    
+    TestServiceImpl t2 = sp.GetService<TestServiceImpl>();
+    t2.Name = "Test2";
+    bool sameInstance = ReferenceEquals(t, t2);
+    t2.SayHi();
+    t.SayHi();
+    Console.WriteLine($"Are t and t2 the same instance? {sameInstance}");
+    */
+
+    ITestService aux;
+
+    using (IServiceScope scope = sp.CreateScope())
+    {
+        ITestService t = sp.GetService<ITestService>();
+        t.Name = "Test";
+        t.SayHi();
+    
+        ITestService t2 = sp.GetService<ITestService>();
+        t2.Name = "Test2";
+        bool sameInstance = ReferenceEquals(t, t2);
+        t2.SayHi();
+        t.SayHi();
+        Console.WriteLine($"Are t and t2 the same instance? {sameInstance}");
+        aux = t2; // Keep a reference to t2 for later use
+    }
+    
+    using (IServiceScope scope2 = sp.CreateScope())
+    {
+        ITestService t3 = scope2.ServiceProvider.GetService<ITestService>();
+        //ITestService t3 = sp.GetRequiredService<ITestService>();
+        t3.Name = "Test3";
+        t3.SayHi();
+        
+        aux.SayHi();
+        bool sameInstance2 = ReferenceEquals(t3, aux);
+        t3.SayHi();
+        Console.WriteLine($"Are t3 and t2 the same instance? {sameInstance2}");
+        Console.WriteLine(t3.GetType());
+    }
+
+    
+}
+
+
+
+public class TestServiceImpl : IDisposable, ITestService
+{
+    public string? Name { get; set; }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, {Name ?? "there"}!");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Disposing {Name ?? "TestServiceImpl"}");
+    }
+}
+
+public interface ITestService
+{
+    string? Name { get; set; }
+    void SayHi();
+}
