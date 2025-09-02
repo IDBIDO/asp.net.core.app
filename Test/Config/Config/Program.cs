c4b24ff34e82f3be97de9fee7d30205108608b05
hechen <hechen@etg.global>
2025-09-02 13:20:56 +0200

    cc

---
+++ Test/Config/Config/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+configBuilder.AddJsonFile("config.json", optional: true, reloadOnChange: true);
+IConfigurationRoot configurationRoot = configBuilder.Build();
+
+string name = configurationRoot["name"];
+Console.WriteLine($"Name from config: {name}");
+string add = configurationRoot.GetSection("proxy:address").Value;
+Console.WriteLine($"Proxy Address from config: {add}");
+
+
+Proxy proxy = configurationRoot.GetSection("proxy").Get<Proxy>();
+Console.WriteLine("Proxy configuration loaded");
+Console.WriteLine($"Address: {proxy.Address} Port: {proxy.Port}");
+
+class Proxy
+{
+    public string Address { get; set; }
+    public int Port { get; set; }
+}
